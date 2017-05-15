7407d844926a67958d919a0c5fc6476e64645e5c
Ilan Copelyn <ilan@netreturn.co.za>
2017-05-15 20:33:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
